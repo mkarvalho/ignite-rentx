198221d190f8da6296f444aa7fd58b7d65b90e6f
Matheus Carvalho <carvalhomath95@gmail.com>
2021-04-30 00:47:32 -0300

    finish chapter IV

---
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -0,0 +1,9 @@
+import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
+
+import { Rental } from "../infra/typeorm/entities/Rental";
+
+export interface IRentalsRepository {
+  findOpenRentalByCar(car_id: string): Promise<Rental>;
+  findOpenRentalByUser(user_id: string): Promise<Rental>;
+  create(data: ICreateRentalDTO): Promise<Rental>;
+}
---
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -0,0 +1,60 @@
+import { inject, injectable } from "tsyringe";
+
+import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { AppError } from "@shared/errors/AppError";
+
+@injectable()
+export class CreateRentalUseCase {
+  constructor(
+    @inject("RentalsRepository")
+    private rentalsRepository: IRentalsRepository,
+
+    @inject("DayjsDateProvider")
+    private dateProvider: IDateProvider
+  ) {}
+
+  async execute({
+    user_id,
+    car_id,
+    expected_return_date,
+  }: ICreateRentalDTO): Promise<Rental> {
+    const minimunHour = 24;
+
+    const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
+      car_id
+    );
+
+    if (carUnavailable) {
+      throw new AppError("Car is unavailable");
+    }
+
+    const rentalOpenToUser = await this.rentalsRepository.findOpenRentalByUser(
+      user_id
+    );
+
+    if (rentalOpenToUser) {
+      throw new AppError("There is a rental in progress for the user!");
+    }
+
+    const dateNow = this.dateProvider.dateNow();
+    const compare = this.dateProvider.compareInHours(
+      dateNow,
+      expected_return_date
+    );
+
+    if (compare < minimunHour) {
+      throw new AppError("Invalid return time!");
+    }
+
+    const rental = await this.rentalsRepository.create({
+      user_id,
+      car_id,
+      expected_return_date,
+    });
+
+    return rental;
+  }
+}
